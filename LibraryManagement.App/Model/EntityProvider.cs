03dce24a1a2b5c0ff136c85a592fc69fb45f1641
Khanh Lam <quockhanhdev@gmail.com>
2020-11-21 23:13:36 +0700

    - Add database script - Add entitry model - Update namespace

---
+++ LibraryManagement.App/Model/EntityProvider.cs
@@ -0,0 +1,61 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Windows;
+
+namespace LibraryManagement.Model
+{
+   public class EntityProvider
+   {
+      public static EntityProvider Instance
+      {
+         get
+         {
+            instance = instance ?? new EntityProvider();
+            return instance;
+         }
+         set => instance = value;
+      }
+
+      public LibraryManagementEntities DbEntities { get; set; }
+
+      public void Reload()
+      {
+         DbEntities.Dispose();
+         DbEntities = new LibraryManagementEntities();
+      }
+
+      public void SaveEntity(object entity, EntityState entityState, bool reloadDatabase = false)
+      {
+         if (entity == null) { return; }
+
+         try
+         {
+            DbEntities.Entry(entity).State = entityState;
+            Instance.DbEntities.SaveChanges();
+         }
+         catch (DbEntityValidationException e)
+         {
+            foreach (var entityValidationError in e.EntityValidationErrors)
+            {
+               MessageBox.Show("Entity of type \"" + entityValidationError.Entry.Entity.GetType().Name + "\" in state \"" + entityValidationError.Entry.State + "\" has the following validation errors:");
+               foreach (var validationError in entityValidationError.ValidationErrors)
+               {
+                  MessageBox.Show("- Property: \"" + validationError.PropertyName + "\", Error: \"" + validationError.ErrorMessage + "\"");
+               }
+            }
+            throw;
+         }
+
+         Instance.DbEntities.Entry(entity).State = EntityState.Detached;
+
+         if (reloadDatabase) { Reload(); }
+      }
+
+      private EntityProvider()
+      {
+         DbEntities = new LibraryManagementEntities();
+      }
+
+      private static EntityProvider instance;
+   }
+}
