6ae641d6183577090b651f473156982e0189fa79
QuocNam <namtranquoc322@gmail.com>
2020-12-16 17:31:44 +0700

    Update ViewModel for LoginWindow, AdminWindow,LibrarianWindow and MemberWindow

---
+++ LibraryManagement.App/Model/ExtensionEntityModel/User.cs
@@ -0,0 +1,13 @@
+﻿using LibraryManagement.Utils;
+using System.Windows;
+using System.Windows.Media;
+
+namespace LibraryManagement.Model
+{
+   public partial class User
+   {
+      public string FullName { get => (this.LastName + " " + this.FirstName).TrimCheck(); }
+      public string GenderDisplay { get => Definition.User.GenderToString(this.Gender); set => Gender = Definition.User.StringToGender(value); }
+      public ImageSource ImageDisplay { get => Base64Utils.Decode.ToImageSource(this.Image) ?? Application.Current.TryFindResource("DefaultUserImage") as ImageSource; }
+   }
+}
