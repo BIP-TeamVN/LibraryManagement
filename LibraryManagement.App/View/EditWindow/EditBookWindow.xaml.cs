73b093d6e2f45d548e4638104f3b76edbce183ef
hohuyhoangg <hoangho1147@gmail.com>
2020-12-22 10:57:25 +0700

    Add Book Window ViewModel
    - Add Base Object
    
    Co-Authored-By: Nguyen Tran Phuc <70422425+Nguyen-Tran-Phuc@users.noreply.github.com>

---
+++ LibraryManagement.App/View/EditWindow/EditBookWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using LibraryManagement.Model;
+using LibraryManagement.ViewModel;
+using System.Windows;
+
+namespace LibraryManagement.View
+{
+    /// <summary>
+    /// Interaction logic for EditBookWindow.xaml
+    /// </summary>
+    public partial class EditBookWindow : Window
+    {
+        public EditBookWindow()
+        {
+            InitializeComponent();
+            txtTitle.Focus();
+        }
+
+        public static bool Show(BookInfo editBookInfo = null)
+        {
+            var editDataContext = new EditBookWindowViewModel(editBookInfo);
+            var addWindow = new EditBookWindow()
+            {
+                DataContext = editDataContext
+            };
+            addWindow.ShowDialog();
+
+            return editDataContext.EditResult;
+        }
+    }
+}
