d0a0362db4387f51f7e60506d45114dbeb499fb6
hohuyhoangg <hoangho1147@gmail.com>
2021-01-15 08:51:04 +0700

    Revert "Merge pull request #21 from HKNP-Team/phuc/figbug_view_model"
    This reverts commit 2c9cacab56accd685d3ae52e6d643e090dbab60b, reversing
    changes made to e1720e7e883f447760ef8b32c8e29cc6a86a446e.

--- LibraryManager.EntityFramework/ViewModel/PageUC/PageMemberBorrowListVM.cs
+++ LibraryManager.EntityFramework/ViewModel/PageUC/PageMemberBorrowListVM.cs
@@ -1,18 +1,6 @@
-using LibraryManager.EntityFramework.Model;
-using LibraryManager.Utils;
-using System.Collections.ObjectModel;
-
-namespace LibraryManager.EntityFramework.ViewModel
+﻿namespace LibraryManager.EntityFramework.ViewModel
 {
-   public class PageMemberBorrowListVM : BaseViewModel
-   {
-      public ObservableCollection<BorrowDTO> ListBookBorrow { get => listBookBorrow; set { listBookBorrow = value; OnPropertyChanged(); } }
-
-      public PageMemberBorrowListVM(string memberId)
-      {
-         ListBookBorrow = BorrowDAL.Instance.GetListByMemberId(memberId);
-      }
-
-      private ObservableCollection<BorrowDTO> listBookBorrow;
+   public class PageMemberBorrowListVM {
+      public PageMemberBorrowListVM(string id) { }
    }
 }
